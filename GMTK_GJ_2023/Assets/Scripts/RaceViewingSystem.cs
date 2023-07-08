513b61f302faf4bb2e7ff260fa1e505995a117b5
Brett <babsportsfan@gmail.com>
2023-07-07 23:03:06 -0400

    Camera Switching Implemented

---
+++ GMTK_GJ_2023/Assets/Scripts/RaceViewingSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class RaceViewingSystem : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera _vcamDriverPov;
+    [SerializeField] CinemachineVirtualCamera _vcamThirdPerson;
+
+    private void OnEnable()
+    {
+        ViewingCameraSwitcher.Register(_vcamThirdPerson);
+        ViewingCameraSwitcher.Register(_vcamDriverPov);
+        ViewingCameraSwitcher.SwitchCamera(_vcamThirdPerson);
+    }
+
+    private void OnDisable()
+    {
+        ViewingCameraSwitcher.UnRegister(_vcamThirdPerson);
+        ViewingCameraSwitcher.UnRegister(_vcamDriverPov);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (ViewingCameraSwitcher.IsActiveCamera(_vcamThirdPerson))
+            {
+                ViewingCameraSwitcher.SwitchCamera(_vcamDriverPov);
+            }
+            else if (ViewingCameraSwitcher.IsActiveCamera(_vcamDriverPov))
+            {
+                ViewingCameraSwitcher.SwitchCamera(_vcamThirdPerson);
+            }
+        }
+    }
+}
